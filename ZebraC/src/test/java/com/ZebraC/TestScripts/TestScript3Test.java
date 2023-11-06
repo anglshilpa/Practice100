32d0fd9a3ae822967c6d8a97f80daadf93ac14f6
anglshilpa <anglshilpa11@gmail.com>
2023-11-06 10:24:46 +0530

    testscript3 commit2

---
+++ ZebraC/src/test/java/com/ZebraC/TestScripts/TestScript3Test.java
@@ -0,0 +1,12 @@
+package com.ZebraC.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class TestScript3Test {
+     @Test
+     public void tc1()
+     {
+    	 System.out.println("hello");
+     }
+     
+}
